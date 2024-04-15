5a2d7f8b86b3b7cee9e7a49499b0b473d994b3be
Leoyna <Leoynaaa2@gmail.com>
2024-04-15 12:48:48 +0200

    label waren noch falsch

--- frontend/kumu_ui/src/components/vues/LandingPage.vue
+++ frontend/kumu_ui/src/components/vues/LandingPage.vue
@@ -6,19 +6,19 @@
         <hr/>
         <div class="login-form-group">
           <label for="exampleInputEmail1">Anmeldename</label>
-          <input style="font-size: 2vmin;" type="text" class="form-control" v-tooltip="{ value: 'Email oder Anmeldename eingeben', showDelay: 1000, hideDelay: 300}" aria-label="Anmeldename" v-model="Anmeldename" placeholder="Anmeldename">
+          <input style="font-size: 2vmin;" type="text" class="form-control" v-tooltip="{ value: 'Email oder Anmeldename eingeben', showDelay: 1000, hideDelay: 300}" aria-label="Anmeldename" v-model="username" placeholder="Anmeldename">
         </div>
         <div class="login-form-group">
           <label for="exampleInputPassword1">Passwort</label>
-          <input style="font-size: 2vmin;" type="password" class="form-control" v-tooltip="{ value: 'Passwort eingeben', showDelay: 1000, hideDelay: 300}" id="exampleInputPassword1" placeholder="Passwort">
+          <input style="font-size: 2vmin;" type="password" class="form-control" v-tooltip="{ value: 'Passwort eingeben', showDelay: 1000, hideDelay: 300}"  v-model="password" id="exampleInputPassword1" placeholder="Passwort">
           <small id="Anmeldehilfe" class="form-text text-muted"></small>
         </div>
         <div class="login-form-group">
           <input type="checkbox" class="form-check-input" id="exampleCheck1">
-          <label class="form-check-label" for="exampleCheck1" style="font-size: 2vmin;">Check me out</label>
+          <label class="form-check-label" for="exampleCheck1" style="font-size: 2vmin;">Eingeloggt bleiben</label>
         </div>
         <div class="login-form-group ">
-          <button v-if="this.validateInput() == true" v-on:click="dummy1" v-tooltip="{ value: 'Create a new Game', showDelay: 1000, hideDelay: 300 }" class="btn btn-dark btn-Login" type="Submit" id="new-game" aria-expanded="false">Anmelden</button>
+          <button v-if="this.validateInput() == true" v-on:click="dummy1" class="btn btn-dark btn-Login" type="Submit" id="new-game" aria-expanded="false">Anmelden</button>
           <button v-else v-on:click="dummy1" v-tooltip="{ value: 'Bitte Anmeldename und Passwort eingeben',pt: {text: 'text-danger'}}" class="btn btn-dark btn-Login" type="Submit" id="new-game" aria-expanded="false">Anmelden</button>
         </div>
       </form>
@@ -42,11 +42,7 @@ export default {
   data() {
     return {
       username: null,
-      intent: null,
-      game: null,
-      popover1: "display: none",
-      popover2: "display: none",
-      popover3: "display: none",
+      password: null,
     };
   },
 
@@ -60,6 +56,15 @@ export default {
       }
 
       if (!regex.test(this.username)) {
+        return;
+      }
+
+      if (!this.password) {
+        //add error handling -> user input needed
+        return;
+      }
+
+      if (!regex.test(this.password)) {
         return;
       }
 
