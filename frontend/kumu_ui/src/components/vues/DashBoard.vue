60dbc454313eecce0a6664a62f1186fb76c98ec1
sven42xyz <101597343+sven42xyz@users.noreply.github.com>
2024-04-16 21:33:49 +0200

    there ya go bruh

--- frontend/kumu_ui/src/components/vues/DashBoard.vue
+++ frontend/kumu_ui/src/components/vues/DashBoard.vue
@@ -1,12 +1,14 @@
 <template>
-    <div id = "Main">
+    <div id="Main">
         <TabMenu :model="items">
             <template #item="{ item, props }">
-                <div v-if="item.type"  v-ripple v-bind="props.action" @click="navigate(item.url)" style="border-color: transparent;">
+                <div v-if="item.type" v-ripple v-bind="props.action" @click="navigate(item.url)"
+                    style="border-color: transparent;">
                     <span v-bind="props.icon" style="color: 'var(--primary-color)'; font-size: 2.25vmin;" />
                     <span v-bind="props.label" style="font-size: 2.25vmin;">{{ item.label }}</span>
                 </div>
-                <div v-else  v-ripple v-bind="props.action" @click="navigate(item.url)" style="position: absolute; right: 0; border-color: transparent;">
+                <div v-else v-ripple v-bind="props.action" @click="navigate(item.url)"
+                    style="position: absolute; right: 0; border-color: transparent;">
                     <span v-bind="props.label" style="margin-right: 1vmin; font-size: 2vmin;">{{ item.label }}</span>
                     <span v-bind="props.icon" style="color: 'var(--primary-color)'; font-size: 2.25vmin;" />
                 </div>
@@ -14,17 +16,22 @@
         </TabMenu>
         <Field-set style="width: 45%; margin-left: 2.5%; margin-top: 1%; background-color: whitesmoke;">
             <template #legend>
-                <div class="flex pl-2" style="background-color: rgb(124, 123, 123); border-top-left-radius: 1vmin; border-top-right-radius: 1vmin; text-align: left; text-indent: 2.5%; font-size: 2.5vmin;">Eingehend</div>
+                <div class="flex pl-2"
+                    style="background-color: rgb(124, 123, 123); border-top-left-radius: 1vmin; border-top-right-radius: 1vmin; text-align: left; text-indent: 2.5%; font-size: 2.5vmin;">
+                    Eingehend</div>
             </template>
             <p class="m-0" style="color: black;">
-                <DataTable :value="products" paginator :rows="5" :rowsPerPageOptions="[5, 10, 20]" style="color: black; font-size: 2vmin; border-radius: 0%;"
-                paginatorTemplate="FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink RowsPerPageDropdown"        currentPageReportTemplate="{first} to {last} of {totalRecords}">
-                    <ColumnColumn field="Rechnungsnummer" header="Rechnungsnummer" style="background-color: whitesmoke;color: black; font-size: 2vmin;"></ColumnColumn>
+                <DataTable :value="products" paginator :rows="5" :rowsPerPageOptions="[5, 10, 20]"
+                    style="color: black; font-size: 2vmin; border-radius: 0%;"
+                    paginatorTemplate="FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink RowsPerPageDropdown"
+                    currentPageReportTemplate="{first} bis {last} von {totalRecords}">
+                    <ColumnColumn field="Rechnungsnummer" header="Rechnungsnummer"
+                        style="background-color: whitesmoke;color: black; font-size: 2vmin;"></ColumnColumn>
                 </DataTable>
             </p>
         </Field-set>
     </div>
-  </template>
+</template>
   
   <script> 
   export default {
