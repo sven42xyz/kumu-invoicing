56649ba4f3ea56b7e602abfef2aa4ef35debf420
Leoyna <Leoynaaa2@gmail.com>
2024-04-17 20:05:52 +0200

    Css stuff

--- frontend/kumu_ui/src/components/vues/DashBoard.vue
+++ frontend/kumu_ui/src/components/vues/DashBoard.vue
@@ -4,29 +4,45 @@
             <template #item="{ item, props }">
                 <div v-if="item.type" v-ripple v-bind="props.action" @click="navigate(item.url)"
                     style="border-color: transparent;">
-                    <span v-bind="props.icon" style="color: 'var(--primary-color)'; font-size: 2.25vmin;" />
-                    <span v-bind="props.label" style="font-size: 2.25vmin;">{{ item.label }}</span>
+                    <span v-bind="props.icon" style="color: 'var(--primary-color)'; font-size: 2.2vmin;" />
+                    <span v-bind="props.label" style="font-size: 2.2vmin;">{{ item.label }}</span>
                 </div>
                 <div v-else v-ripple v-bind="props.action" @click="navigate(item.url)"
                     style="position: absolute; right: 0; border-color: transparent;">
                     <span v-bind="props.label" style="margin-right: 1vmin; font-size: 2vmin;">{{ item.label }}</span>
-                    <span v-bind="props.icon" style="color: 'var(--primary-color)'; font-size: 2.25vmin;" />
+                    <span v-bind="props.icon" style="color: 'var(--primary-color)'; font-size: 2.2vmin;" />
                 </div>
             </template>
         </TabMenu>
-        <Field-set style="width: 45%; margin-left: 2.5%; margin-top: 1%; background-color: whitesmoke;">
+        <Field-set style="width: 47%; margin-left: 2%; float:left; margin-top: 1%; background-color: whitesmoke;">
             <template #legend>
                 <div class="flex pl-2"
                     style="background-color: rgb(124, 123, 123); border-top-left-radius: 1vmin; border-top-right-radius: 1vmin; height: 6vmin; padding: 1vmin; text-align: left; text-indent: 2.5%; font-size: 2.5vmin;">
                     Eingehend</div>
             </template>
             <p class="m-0" style="color: black;">
-                <DataTable :value="products" paginator :rows="5" :rowsPerPageOptions="[5, 10, 20]"
-                    style="color: black; font-size: 2vmin; border-radius: 0%;"
+                <DataTable :value="products" stripedRows paginator :rows="15"
+                    style="color: black; border-radius: 0%;"
                     paginatorTemplate="FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink RowsPerPageDropdown"
                     currentPageReportTemplate="{first} bis {last} von {totalRecords}">
                     <ColumnColumn field="Rechnungsnummer" header="Rechnungsnummer"
-                        style="background-color: whitesmoke;color: black; font-size: 2vmin;"></ColumnColumn>
+                        style="color: black; font-size: 1.75vmin; padding: 1vmin"></ColumnColumn>
+                </DataTable>
+            </p>
+        </Field-set>
+        <Field-set style="width: 47%; float:right;margin-right: 2%; margin-top: 1%; background-color: whitesmoke;">
+            <template #legend>
+                <div class="flex pl-2"
+                    style="background-color: rgb(124, 123, 123); border-top-left-radius: 1vmin; border-top-right-radius: 1vmin; height: 6vmin; padding: 1vmin; text-align: left; text-indent: 2.5%; font-size: 2.5vmin;">
+                    Ausgehend</div>
+            </template>
+            <p class="m-0" style="color: black;">
+                <DataTable :value="products"  stripedRows paginator :rows="15"
+                    style="color: black; border-radius: 0%;"
+                    paginatorTemplate="FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink RowsPerPageDropdown"
+                    currentPageReportTemplate="{first} bis {last} von {totalRecords}">
+                    <ColumnColumn field="Rechnungsnummer" header="Rechnungsnummer"
+                        style="color: black; font-size: 1.75vmin; padding: 1vmin"></ColumnColumn>
                 </DataTable>
             </p>
         </Field-set>
@@ -50,7 +66,7 @@
                 {type: '', label: 'Abmelden ', url: '/', icon:'pi pi-sign-out'},
             ],
         products: [
-            {Rechnungsnummer: 'Rechnungsnummer'}, {Rechnungsnummer: 'Rechnungsnummer'}
+            {Rechnungsnummer: 'Rechnungsnummer'}, {Rechnungsnummer: 'Rechnungsnummer'}, {Rechnungsnummer: 'Rechnungsnummer'}, {Rechnungsnummer: 'Rechnungsnummer'}, {Rechnungsnummer: 'Rechnungsnummer'}, {Rechnungsnummer: 'Rechnungsnummer'}, {Rechnungsnummer: 'Rechnungsnummer'}, {Rechnungsnummer: 'Rechnungsnummer'}, {Rechnungsnummer: 'Rechnungsnummer'}, {Rechnungsnummer: 'Rechnungsnummer'}, {Rechnungsnummer: 'Rechnungsnummer'}, {Rechnungsnummer: 'Rechnungsnummer'}, {Rechnungsnummer: 'Rechnungsnummer'}, {Rechnungsnummer: 'Rechnungsnummer'}
         ]
       };
     },
