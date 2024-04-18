3087a7cd8a175c94afcd2f56f1ea7173ad36a0d5
Leoyna <Leoynaaa2@gmail.com>
2024-04-18 21:07:44 +0200

    ist noch hässlich aber hey, es filtert

---
+++ frontend/kumu_ui/src/components/vues/InvoiceUpload.vue
@@ -0,0 +1,127 @@
+<template>
+    <div id="Main">
+        <TabMenu :model="items">
+            <template #item="{ item, props }">
+                <div v-if="item.type" v-ripple v-bind="props.action" @click="navigate(item.url)"
+                    style="border-color: transparent;">
+                    <span v-bind="props.icon" style="color: 'var(--primary-color)'; font-size: 2.2vmin;" />
+                    <span v-bind="props.label" style="font-size: 2.2vmin;">{{ item.label }}</span>
+                </div>
+                <div v-else v-ripple v-bind="props.action" @click="navigate(item.url)"
+                    style="position: absolute; right: 0; border-color: transparent;">
+                    <span v-bind="props.label" style="margin-right: 1vmin; font-size: 2vmin;">{{ item.label }}</span>
+                    <span v-bind="props.icon" style="color: 'var(--primary-color)'; font-size: 2.2vmin;" />
+                </div>
+            </template>
+        </TabMenu>
+        <Card style="width: 47%; height: 5vmin; margin-left: 2%; float:left; margin-top: 1%; background-color: whitesmoke; border-radius: 1vmin;">
+            hi
+        </Card>
+        <Card style="width: 47%; height: 5vmin; margin-right: 2%; float:right; margin-top: 1%; background-color: whitesmoke; border-radius: 1vmin;">
+            hi
+        </Card>
+        <Field-set style="width: 47%; margin-left: 2%; float:left; margin-top: 1%; background-color: whitesmoke;">
+            <template #legend>
+                <div class="flex pl-2"
+                    style="background-color: rgb(124, 123, 123); border-top-left-radius: 1vmin; border-top-right-radius: 1vmin; height: 6vmin; padding: 1vmin; text-align: left; text-indent: 2.5%; font-size: 2.5vmin;">
+                    Eingehend</div>
+            </template>
+            <p class="m-0" style="color: black;">
+                <DataTable v-model:filters="filters" :value="products" dataKey="id" filterDisplay="row" stripedRows scrollable scrollHeight="73vmin" style="color: black; border-radius: 0%; height: 80%">
+                    <ColumnColumn field="Rechnungsnummer" filterField="Rechnungsnummer" header="Rechnungsnummer" style="color: black; font-size: 1.75vmin; padding: 1vmin">
+                        <template #body="{ data }">{{ data.Rechnungsnummer }}</template>
+                        <template #filter="{ filterModel, filterCallback }">
+                            <InputText v-model="filterModel.value" type="text" @input="filterCallback()" class="p-column-filter" placeholder="Rechnungsnummer" />
+                        </template>
+                    </ColumnColumn>
+                    <ColumnColumn field="Partner" filterField="Partner" header="Partner" style="color: black; font-size: 1.75vmin; padding: 1vmin">
+                        <template #body="{ data }">{{ data.Partner }}</template>
+                        <template #filter="{ filterModel, filterCallback }">
+                            <InputText v-model="filterModel.value" type="text" @input="filterCallback()" class="p-column-filter" placeholder="Partner" />
+                        </template>
+                    </ColumnColumn>
+                    <ColumnColumn field="Datum" filterField="Datum" header="Datum" style="color: black; font-size: 1.75vmin; padding: 1vmin">
+                        <template #body="{ data }">{{ data.Datum }}</template>
+                        <template #filter="{ filterModel, filterCallback }">
+                            <InputText v-model="filterModel.value" type="text" @input="filterCallback()" class="p-column-filter" placeholder="Datum" />
+                        </template>
+                    </ColumnColumn>
+                    <ColumnColumn field="Summe" filterField="Summe" header="Summe" style="color: black; font-size: 1.75vmin; padding: 1vmin">
+                        <template #body="{ data }">{{ data.Summe }}</template>
+                        <template #filter="{ filterModel, filterCallback }">
+                            <InputText v-model="filterModel.value" type="text" @input="filterCallback()" class="p-column-filter" placeholder="Summe" />
+                        </template>
+                    </ColumnColumn>
+                </DataTable>
+            </p>
+        </Field-set>
+        <Card style="width: 47%; height: 80%; margin-right: 2%; float:right; margin-top: 1%; background-color: whitesmoke; border-radius: 1vmin;">
+            hi
+        </Card>
+    </div>
+</template>
+  
+  <script> 
+  import { FilterMatchMode } from 'primevue/api';
+  export default {
+    name: 'DashBoard',
+  
+    created(){
+    },
+    
+  
+    data() {
+      return {
+        filters: {
+                Rechnungsnummer: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+                Partner: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+                Datum: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+                Summe: { value: null, matchMode: FilterMatchMode.STARTS_WITH }
+            },
+            items: [
+                {type: 'label', label: 'Übersicht', url: '/dashboard', icon:'pi pi-home'},
+                {type: 'label', label: 'Rechnungseingabe', url: '/outbound', icon:''},
+                {type: 'label', label: 'Rechnungseingang', url: '/inbound', icon:''},
+                {type: 'label', label: 'Stammdatenverwaltung', url: '/contacts', icon:''},
+                {type: '', label: 'Abmelden ', url: '/', icon:'pi pi-sign-out'},
+            ],
+        products: [
+            {Rechnungsnummer: '012345678',
+             Partner: 'Hans',
+             Datum: '23.01.2024',
+             Summe: '1300€'  
+            }, 
+            {Rechnungsnummer: '547654576',
+             Partner: 'Anna',
+             Datum: '23.02.2024',
+             Summe: '222€'  
+            },
+            {Rechnungsnummer: '462354645',
+             Partner: 'Lina',
+             Datum: '23.02.2024',
+             Summe: '300€'  
+            },
+        ],
+      };
+    },
+  
+    methods: {
+        navigate(i){
+            this.$router.push(i);    
+        }
+  
+    }
+  }
+  </script>
+  
+  <style scoped>
+   #Main{
+    width: 100%;
+    height: 100%;
+   }
+ 
+   .p-paginator {
+        background: whitesmoke;
+        color: grey;
+    }
+  </style>
--- frontend/kumu_ui/src/router/index.js
+++ frontend/kumu_ui/src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LandingPage from '../components/vues/LandingPage.vue'
 import DashBoard from '../components/vues/DashBoard.vue'
+import InvoiceUpload from '../components/vues/InvoiceUpload.vue'
 
 const routes = [
     { path: '/', component: LandingPage },
     { path: '/dashboard', name: 'dashboard', component: DashBoard },
+    { path: '/inbound', name: 'inbound', component: InvoiceUpload },
 ];
 
 const router = createRouter({
