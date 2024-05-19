569f6788a97854a3bcc538c58395033bb854125d
Leoyna <Leoynaaa2@gmail.com>
2024-05-19 20:47:53 +0200

    and more

--- frontend/kumu_ui/src/components/scraps/NavBar.vue
+++ frontend/kumu_ui/src/components/scraps/NavBar.vue
@@ -3,13 +3,13 @@
         <template #item="{ item, props }">
             <div v-if="item.type" :target="item.target" v-bind="props.action" @click="navigate(item.url)"
                 style="border-color: transparent;">
-                <span v-bind="props.icon" style="color: 'var(--primary-color)'; font-size: 2.2vmin;" />
-                <span v-bind="props.label" style="font-size: 2.2vmin;">{{ item.label }}</span>
+                <span v-bind="props.icon" style="color: 'var(--primary-color)'; font-size: 2vmin;" />
+                <span v-bind="props.label" style="font-size: 2vmin;">{{ item.label }}</span>
             </div>
             <div v-else :target="item.target" v-bind="props.action" @click="navigate(item.url)"
                 style="position: absolute; right: 0; border-color: transparent;">
                 <span v-bind="props.label" style="margin-right: 1vmin; font-size: 2vmin;">{{ item.label }}</span>
-                <span v-bind="props.icon" style="color: 'var(--primary-color)'; font-size: 2.2vmin;" />
+                <span v-bind="props.icon" style="color: 'var(--primary-color)'; font-size: 2vmin;" />
             </div>
         </template>
     </TabMenu>
@@ -35,9 +35,9 @@
       return {
         items: [
                 {type: 'label', label: 'Übersicht', url: '/dashboard', icon:'pi pi-home'},
-                {type: 'label', label: 'Rechnungseingabe', url: '/outbound', icon:''},
-                {type: 'label', label: 'Rechnungseingang', url: '/inbound', icon:''},
-                {type: 'label', label: 'Stammdatenverwaltung', url: '/contacts', icon:''},
+                {type: 'label', label: 'Rechnungseingabe', url: '/outbound', icon:'pi pi-upload'},
+                {type: 'label', label: 'Rechnungseingang', url: '/inbound', icon:'pi pi-download'},
+                {type: 'label', label: 'Stammdatenverwaltung', url: '/contacts', icon:'pi pi-address-book'},
                 {type: '', label: 'Abmelden ', url: '/', icon:'pi pi-sign-out'},
             ]
       };
